b536745ec9114a1ddabe546616be728b846d56c5
Edvard Thörnros <edvard.thornros@gmail.com>
2021-01-19 18:51:05 +0100

    Change to new way of structuring tasks

---
+++ answers.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+
+# TODO(ed): Ask if you REALLY want to see the answers?
+
+case $1 in
+1*|01*)
+    echo "git add ."
+
+    exit 0
+;;
+
+2*|02*)
+    echo "echo \"a new line\" >> hello.txt"
+    echo "git add ."
+    echo "git commit -m \"do the thing!\""
+
+    exit 0
+;;
+
+3*|03*)
+    echo "git add ."
+    echo "git commit -m \"do the thing!\""
+
+    exit 0
+;;
+
+4*|04*)
+    echo "git switch -c \"another-branch\""
+
+    exit 0
+;;
+
+
+esac
+
+echo "Don't know what task that is..."
+echo "Possible exercises:"
+ls .
+exit 1
---
+++ check.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env sh
+
+case $1 in
+1*|01*)
+    cd 01-*
+    
+    if ! git diff --quiet
+    then
+        echo "There are unstaged changes"
+        exit 1
+    fi
+    echo "SOLVED"
+    exit 0
+;;
+
+2*|02*)
+    cd 02-*
+
+    if ! git diff --quiet
+    then
+        echo "There are unstaged changes"
+        exit 1
+    fi
+
+    NR=$(git rev-list --count HEAD)
+    if test $NR -lt 2
+    then
+        echo "I didn't find any new commits"
+        exit 1
+    fi
+
+    echo "SOLVED"
+    exit 0
+;;
+
+3*|03*)
+    cd 03-*
+
+    if ! git diff --quiet
+    then
+        echo "There are unstaged changes"
+        exit 1
+    fi
+
+    NR=$(git rev-list --count HEAD)
+    if test $NR -lt 2
+    then
+        echo "I didn't find any new commits"
+        exit 1
+    fi
+
+    echo "SOLVED"
+    exit 0
+;;
+
+4*|04*)
+    cd 04-*
+
+    if ! git diff --quiet
+    then
+        echo "There are unstaged changes"
+        exit 1
+    fi
+
+    BRANCH=$(git branch --show-current)
+    if test "$BRANCH" = "main"
+    then
+        echo "Looks like you're still on the main branch"
+        exit 1
+    fi
+
+    echo "SOLVED"
+    exit 0
+;;
+
+esac
+
+echo "Don't know what task that is..."
+echo "Possible exercises:"
+ls .
+exit 1
